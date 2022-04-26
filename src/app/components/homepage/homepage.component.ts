b9ba487e9327bbc160b4b16b7ea98c45d2341db1
Shamanth Kumar <shamanth.kumar@ahead.com>
2022-04-26 16:47:06 -0500

    environment files

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-homepage',
@@ -14,7 +15,9 @@ export class HomepageComponent implements OnInit {
 
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('environment: ', environment.apiURL);
+  }
 
   routeToAllProducts() {
     console.log('clicked', this.isAuthenticated);
